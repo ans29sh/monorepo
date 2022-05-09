9fa978b92ccba0865cc1bf8d71f06076bbfc8d5e
ans29sh <103987428+ans29sh@users.noreply.github.com>
2022-05-10 02:54:12 +0530

    Minor changes (ide and gradle)

--- behavioural-engine/src/main/java/com/hugo/BehaviouralEngineApplication.java
+++ behavioural-engine/src/main/java/com/hugo/BehaviouralEngineApplication.java
@@ -3,7 +3,7 @@ package com.hugo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class BehaviouralEngineApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
